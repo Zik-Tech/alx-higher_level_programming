f20f9c82b3d7d762adacafb1c67c14ad5caf91e4
unique-unice <adioeunice0@gmail.com>
2022-11-29 05:23:21 -0800

    update

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -9,6 +9,6 @@ part_1 = f"Last digit of {number} is {last_digit}"
 if last_digit > 5:
     print(f"{part_1} and is greater than 5")
 elif last_digit == 0:
-    print(f"{part_1} abd is 0")
+    print(f"{part_1} and is 0")
 elif last_digit < 6 and last_digit != 0:
     print(f"{part_1} and is less than 6 and not 0")
