a95c6b5915885257dede8ec5577c0ba513fb7069
unique-unice <adioeunice0@gmail.com>
2022-11-29 05:07:38 -0800

    update

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+last_digit = number % 10
+part_1 = f"Last digit of {number} is {last_digit}"
+if last_digit > 5:
+    print(f"{part_1} and is greater than 5")
+elif last_digit == 0:
+    print(f"{part_1} abd is 0")
+elif last_digit < 6 and != 0:
+    print(f"{part_1} and is less than 6 and not 0")
