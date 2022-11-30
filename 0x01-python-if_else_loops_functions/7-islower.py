e9297be02b51559ce3d88fad740ce5ebbaf3dc50
unique-unice <adioeunice0@gmail.com>
2022-11-30 05:12:30 -0800

    update

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -2,7 +2,7 @@
 
 def islower(c):
     """Function to check if a letter is lowercase ""
-    if ord(c) in range(97, 123):
+    if ord(c) >= 97 and ord(c) <= 122:
         return True
     else:
         return False
